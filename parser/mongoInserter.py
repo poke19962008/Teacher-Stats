a63dcf0c93029e63689a506aaf6c532007e42031
Sayan Das <poke19962008@gmail.com>
2016-01-26 21:30:25 +0530

    changed schema

--- parser/mongoInserter.py
+++ parser/mongoInserter.py
@@ -8,6 +8,6 @@ try:
 	con = pymongo.MongoClient(host=mongoURI, port=27017)
 	db = con['teacher']
 	for teacher in data:
-		db.main.insert({ 'name': data[teacher] })
+		db.main.insert({ 'name': teacher, 'data': data[teacher] })
 except:
 	print "[Error] Cannot connect with mongodb."
