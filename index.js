7c3e5ae65981e0a3661a46f29ac50ef8642ca308
Sayan Das <poke19962008@gmail.com>
2016-01-26 22:17:18 +0530

    api fixes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoClient = require('mongodb').MongoClient;
 var mongoURI = "mongodb://localhost:27017/teacher";
 var limit = 5;
 
-app.get('/teacher/query', function(req,res){
+app.get('/teacher/api', function(req,res){
   var query = req.query.q;
 
   mongoClient.connect(mongoURI, function (err, db){
