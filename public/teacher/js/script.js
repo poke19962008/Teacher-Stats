b6395a25fcfe44984c7f8de465da52f50f5aabc1
Sayan Das <poke19962008@gmail.com>
2016-01-27 22:17:46 +0530

    Fuckingg ;

--- public/teacher/js/script.js
+++ public/teacher/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
     $("#title, #search, footer").addClass('animated bounceInUp');
 });
 
-var ranges = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100"]
+var ranges = ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100"];
 
 var options = {
   distributeSeries: true,
